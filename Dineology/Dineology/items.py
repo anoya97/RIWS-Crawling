5fa3aed17ac5101343f006d277008884576a4a19
NOYA <91661771+anoya97@users.noreply.github.com>
2024-10-11 20:38:37 +0200

    Add files via upload

--- Dineology/Dineology/items.py
+++ Dineology/Dineology/items.py
@@ -5,8 +5,10 @@
 
 import scrapy
 
-
 class DineologyItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
+    name = scrapy.Field()
+    direction = scrapy.Field()
+    price = scrapy.Field()
+
     pass
--- Dineology/Dineology/pipelines.py
+++ Dineology/Dineology/pipelines.py
@@ -3,11 +3,35 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
-
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+import json
 
 
-class DineologyPipeline:
+class DineologyJsonPipeline:
+    def open_spider(self, spider):
+        self.file = open('dineology.json', 'w', encoding='utf-8')
+        self.file.write('[')
+        self.first_item = True  # Check if is first
+
+    def close_spider(self, spider):
+        self.file.write(']\n')
+        self.file.close()
+
     def process_item(self, item, spider):
+        # Reemplazar la ñ por n y caracteres especiales en dirección
+        item['direction'] = item['direction'].replace('ñ', 'n').replace('Ñ', 'N')
+
+        # Limpiar cada campo del item antes de escribirlo
+        item = {key: value.strip() if isinstance(value, str) else value for key, value in item.items()}
+
+        if not self.first_item:
+            self.file.write(',\n')
+        self.first_item = False
+
+        # Escribir el item formateado
+        line = json.dumps(dict(item), indent=4, ensure_ascii=False)  # UTF-8 friendly JSON output
+        self.file.write(line)
+
         return item
+
