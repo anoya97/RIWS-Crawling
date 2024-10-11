f9a43c7c47cdbe0a3fd52a38ae1e086c8972112d
NOYA <91661771+anoya97@users.noreply.github.com>
2024-10-11 20:44:50 +0200

    Estructura Spiders

---
+++ Dineology/Dineology/spiders/RestaurantRepsolSpider.py
@@ -0,0 +1,23 @@
+import scrapy
+from ..items import DineologyItem
+
+class RestaurantRepsolSpider(scrapy.Spider):
+    name = "RestaurantRepsolSpider"
+
+    start_urls = ['https://guiarepsol.com/es/soles-repsol/ediciones-de-soles-guia-repsol/']
+
+    def parse(self, response):
+        for restaurant in response.css(''):  # Selecciona cada bloque de restaurante
+
+            item = DineologyItem()
+            item['name'] = ""
+            item['direction'] = ""
+            item['price'] = ""
+
+            yield item
+
+        # Follow the next page
+        next_page = response.css('li.next a::attr(href)').get()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)  # Construir la URL completa
+            yield scrapy.Request(next_page, callback=self.parse)
